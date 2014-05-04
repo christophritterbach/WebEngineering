4d1c3691d305daf4f00ea8362596a2905f674a71
Christoph Ritterbach <hpotsirhC.Rgooglemail.com>
2014-05-04 19:10:29 +0200

    SLIM

--- Examen/app/store/NobelpriceStore.js
+++ Examen/app/store/NobelpriceStore.js
@@ -9,8 +9,8 @@ Ext.define('ArsnovaStatistics.store.NobelpriceStore', {
             startParam: false, //to remove param "start"
             limitParam: false, //to remove param "limit"
             noCache: false, //to remove param "_dc"
-            //url: 'http://christophritterbach.cloudcontrolled.com/Examen/nobelprice.php/nobelprice?year=2013'
-            url: 'http://192.168.42.65/mooc/Examen/nobelprice.php/nobelprice?year=2013',
+            url: 'http://christophritterbach.cloudcontrolled.com/Examen/nobelprice.php/nobelprice?year=2013'
+            //url: 'http://192.168.42.65/mooc/Examen/nobelprice.php/nobelprice?year=2013',
             //url: 'http://api.nobelprize.org/v1/prize.json?category=physics&year=1990',
         },
     }
